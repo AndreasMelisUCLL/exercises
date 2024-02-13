55be82726f0f7276c5d09fdf26d1da7bf882db47
Andreas Melis <andreas.melis@student.ucll.be>
2024-02-13 11:33:38 +0100

    added solutions for 01-02

--- 01-basic-python/02-conditionals/02-card-value/student.py
+++ 01-basic-python/02-conditionals/02-card-value/student.py
@@ -1,1 +1,11 @@
 # Write your code here
+def card_value(string):
+    # facecards = ['Jack', 'Queen', 'King']  
+    # for i, facecard in enumerate(facecards):
+    #     if string == facecard:
+    #         return 11 + i 
+    if string == 'Jack': return 11
+    if string == 'Queen': return 12
+    if string == 'King': return 13
+    if string == 'Ace': return 1
+    return int(string)
