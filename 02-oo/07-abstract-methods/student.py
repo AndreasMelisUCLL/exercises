204588e7f0f96f6fcd0d15b1b4766f67b3944be9
Andreas Melis <andreas.melis@student.ucll.be>
2024-03-09 13:35:23 +0100

    Add solution for 02.01 and 02.07-02.10

--- 02-oo/07-abstract-methods/student.py
+++ 02-oo/07-abstract-methods/student.py
@@ -1,1 +1,63 @@
 # Write your code here
+from abc import ABC, abstractmethod
+class A(ABC):
+    def a(self):
+        self.b()
+        
+    @abstractmethod
+    def b(self):
+        ...
+        
+    @abstractmethod
+    def c(self):
+        ...
+
+    def e(self):
+        self.c()
+
+
+class B(A): # gets a, e, has to implement b, c
+    def b(self):
+        self.a()
+
+    def c(self):
+        self.e()
+
+
+class C(B): # gets a, b, c, e
+    def f(self):
+        pass
+
+
+class D(A): # gets a, e, has to implement b, c
+    def b(self):
+        self.f()
+        
+    @abstractmethod
+    def f(self):
+        ...
+
+
+class E(D): # gets a, b, e, has to implement c, f
+    def c(self):
+        self.a()
+
+    def f(self):
+        self.e()
+
+    def g(self):
+        self.f()
+
+
+class F(ABC):
+    def a(self):
+        self.b()
+        self.f()
+    
+    @abstractmethod
+    def b(self):
+        ...
+        
+    @abstractmethod
+    def f(self):
+        ...
