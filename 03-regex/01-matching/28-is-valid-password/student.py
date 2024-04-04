5d1514eb40f8f81d3519824f0e63e71428c001cd
Andreas Melis <andreas.melis@student.ucll.be>
2024-04-05 01:56:43 +0200

    Finish 03.01

--- 03-regex/01-matching/28-is-valid-password/student.py
+++ 03-regex/01-matching/28-is-valid-password/student.py
@@ -1,1 +1,25 @@
 # Write your code here
+import re
+
+
+def is_valid_password(string):
+    required_patterns = [
+        r'.{12}',
+        r'[0-9]',
+        r'[a-z]',
+        r'[A-Z]',
+        r'[\+\-\*\/\.@]'
+    ]
+    
+    if not all([re.search(pattern, string) for pattern in required_patterns]):
+        return False
+    
+    invalid_patterns = [
+        r'(.)\1\1',
+        r'(.)(.*\1){3}'
+    ]
+    
+    if any([re.search(pattern, string) for pattern in invalid_patterns]):
+        return False
+    
+    return True
