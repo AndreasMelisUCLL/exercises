956c860ecb41ceca1162f4ada3ba2e2da0c3b6b3
Andreas Melis <andreas.melis@student.ucll.be>
2024-04-08 16:13:33 +0200

    Solve 03.03

--- 03-regex/03-substituting/04-hide-email-addresses/student.py
+++ 03-regex/03-substituting/04-hide-email-addresses/student.py
@@ -1,1 +1,9 @@
 # Write your code here
+import re
+
+
+def hide_email_addresses(string):
+    def replace(match):
+        return '*' * len(match.group(1))
+    
+    return re.sub(r'\b([a-zA-Z0-9.]+@[a-zA-Z0-9.]+)\b', replace, string)
